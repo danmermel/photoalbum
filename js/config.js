bb9cb863366f6585a20527e11c47426c10f79811
danmermel <danmermel@gmail.com>
2018-06-26 21:41:48 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-varRedirectUriSignIn =  'https://mermelstein.co.uk/album.html';
+var RedirectUriSignIn =  'https://mermelstein.co.uk/album.html';
 var RedirectUriSignOut = 'https://mermelstein.co.uk/album.html'
 
 // var	RedirectUriSignIn =  'http://localhost:8001/album.html';
